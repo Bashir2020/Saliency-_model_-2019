641ad6674210f0240ad8d540e2483ffcd243146d
Bashir Ghariba <52217697+Bashir2020@users.noreply.github.com>
2019-07-06 19:45:22 -0230

    Update readImages.m

--- readImages.m
+++ readImages.m
@@ -1,11 +1,9 @@
 function data = readImages(filename)
-%READDATASTOREIMAGE Read file formats supported by IMREAD.
+%----READDATASTOREIMAGE Read file formats supported by IMREAD.
  
-%   See also matlab.io.datastore.ImageDatastore, datastore, mapreduce.
+%  --- See also matlab.io.datastore.ImageDatastore, datastore, mapreduce.
  
-%   Copyright 2016 The MathWorks, Inc.
- 
-% Turn off warning backtrace before calling imread
+% -----Turn off warning backtrace before calling imread
 onState = warning('off', 'backtrace');
 c = onCleanup(@() warning(onState));
 data = imread(filename);
