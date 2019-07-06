66a7bef7f157cf38b9bd4d4d938211aa19a11fbf
Bashir Ghariba <52217697+Bashir2020@users.noreply.github.com>
2019-07-06 19:40:46 -0230

    Update Test.m

--- Test.m
+++ Test.m
@@ -1,10 +1,3 @@
-% load('C:\Users\bashir\Desktop\UBMG.mat')
-% load('C:\Users\bashir\Desktop\bmg1000-16.mat')
-% load('C:\Users\bashir\Desktop\bmg9000-16.mat')
-% load('C:\Users\bashir\Desktop\bmg1000-19.mat')
-% load('C:\Users\bashir\Desktop\bmg8s.mat')
-% load('C:\Users\bashir\Desktop\bmg16s.mat')
-% load('C:\Users\bashir\Desktop\bmg32s.mat')
 tic;
 clc,close all;
 [imagefile, user_canceled] = imgetfile();
@@ -12,13 +5,15 @@ if user_canceled
     msgbox(sprintf('No image selected. Try Again!'),'Error','Error');
     return
 end
+% ------ read test image------
 I=imread(imagefile);
 figure,
 subplot(1,3,1),imshow(I),title('(Test Image)')
+% ------ semantic segematation---------
 [C,scores] = semanticseg(I,net);
 B = labeloverlay(I, C);
 L = 2-double(C);
-%------------gaussian ---------
+%------------gaussian filter ---------
 h = fspecial('gaussian',[],10); 
 L2 = imfilter(L,h,'same');
 subplot(1,3,2),imshow(B),title('(Human Eye Fixation Prediction)');
